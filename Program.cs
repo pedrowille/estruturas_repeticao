b06fd92e99bb997c512cfbda7e18304a3b1148bd
pedrowille <pedrowille9@gmail.com>
2021-11-26 11:57:55 -0300

    Estruturas de repetição

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EstruturasRepeticao
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+// for (int i=0; i<10; i++)
+    // Console.WriteLine("O valor de I é {i}");
+int contador =0;
+        while (contador <5)
+        {
+Console.WriteLine($"O contador do while é: {contador}");
+contador++;        
+        }
+do
+        {
+            Console.WriteLine($"O valor do contador é {contador}");
+        contador++;
+        } while (contador <=10);
+ List<string> listaNomes= new ();      
+listaNomes.Add("Paulo Ponciano");
+listaNomes.Add("Jefferson");
+listaNomes.Add("rubens");
+listaNomes.Add("Marcos");
+listaNomes.Add("Luciane");
+listaNomes.Add("Mais um monte de nomes aleatórios.");
+foreach (string nome in listaNomes)
+{
+Console.WriteLine($"O nome da lista é: {nome}");
+}        
+        }
+    }
+}
